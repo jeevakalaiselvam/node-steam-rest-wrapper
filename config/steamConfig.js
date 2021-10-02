d09a020f47cdfedc0730fc5e665d4cf106012f8b
jeevakalaiselvam <jeevak001@outlook.com>
2021-10-02 09:34:00 +0530

    Removed HTTPS Endpoint for EC2

--- config/steamConfig.js
+++ config/steamConfig.js
@@ -1,16 +1,16 @@
 const dotenv = require("dotenv");
 dotenv.config();
 
-exports.STEAM_ALL_GAMES_URL = `https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${process.env.STEAM_API_KEY}&steamid=${process.env.STEAM_USER_ID}&format={{RES_FORMAT}}&include_appinfo=true`;
+exports.STEAM_ALL_GAMES_URL = `http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${process.env.STEAM_API_KEY}&steamid=${process.env.STEAM_USER_ID}&format={{RES_FORMAT}}&include_appinfo=true`;
 
 exports.STEAM_ALL_ACHIEVEMENTS_SCHEMA = (gameID) =>
   `http://api.steampowered.com/ISteamUserStats/GetSchemaForGame/v0002/?key=${process.env.STEAM_API_KEY}&appid=${gameID}&format=json&l=english`;
 
 exports.STEAM_ALL_ACHIEVEMENTS_GLOBAL = (gameID) =>
-  `https://api.steampowered.com/ISteamUserStats/GetGlobalAchievementPercentagesForApp/v0002/?gameid=${gameID}&format=json`;
+  `http://api.steampowered.com/ISteamUserStats/GetGlobalAchievementPercentagesForApp/v0002/?gameid=${gameID}&format=json`;
 
 exports.STEAM_ALL_ACHIEVEMENTS_PLAYER = (gameID) =>
-  `https://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid=${gameID}&key=${process.env.STEAM_API_KEY}&steamid=${process.env.STEAM_USER_ID}`;
+  `http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid=${gameID}&key=${process.env.STEAM_API_KEY}&steamid=${process.env.STEAM_USER_ID}`;
 
 exports.STEAM_GAME_HEADER_IMAGE = (gameID) =>
-  `https://cdn.cloudflare.steamstatic.com/steam/apps/${gameID}/header.jpg`;
+  `http://cdn.cloudflare.steamstatic.com/steam/apps/${gameID}/header.jpg`;
