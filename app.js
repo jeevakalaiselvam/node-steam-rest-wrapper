1e0f7f6fad7ff0a901570b3e55803957b6015f30
Jeeva Kalaiselvam <jeevak001@outlook.com>
2021-12-31 23:13:51 +0530

    Testing Achievement

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ dotenv.config();
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
+  res.header('Access-Control-Allow-Methods', '*');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   if ('OPTIONS' == req.method) {
      res.sendStatus(200);
