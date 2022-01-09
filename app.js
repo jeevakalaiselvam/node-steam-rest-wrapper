753a9232ed80080b0e355c7f4be9bf9dd41e6458
Jeeva Kalaiselvam <jeevak001@outlook.com>
2022-01-09 15:07:50 +0530

    Added Cluster

--- app.js
+++ app.js
@@ -1,96 +1,110 @@
-const express = require("express");
-const path = require("path");
-const dotenv = require("dotenv");
-const morgan = require("morgan");
-const rateLimit = require("express-rate-limit");
-const helmet = require("helmet");
-const mongoSanitize = require("express-mongo-sanitize");
-const xss = require("xss-clean");
-const cookieParser = require("cookie-parser");
-const compression = require("compression");
-const cors = require("cors");
-const app = express();
-const apiRouter = require("./routes/apiRoutes.js");
-const {
-  allOwnedGames,
-  getHiddenAchievementDataAndStore,
-  refreshDatabaseAndStore,
-} = require("./controllers/cacheController.js");
-const { log } = require("console");
-const { LOG } = require("./helper/logger.js");
-console.clear();
+var cluster = require("cluster");
+if (cluster.isMaster) {
+  cluster.fork();
 
-//Load in config
-dotenv.config();
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', '*');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  if ('OPTIONS' == req.method) {
-     res.sendStatus(200);
-   }
-   else {
-     next();
-   }});
-
-//Middleware Stack
-app.use(compression());
-app.enable("trust proxy");
-app.options("*", cors());
-app.use(cors());
-app.use(express.static(path.join(__dirname, "public")));
-app.set("view engine", "pug");
-app.set("views", path.join(__dirname, "views"));
-app.use(helmet());
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
+  cluster.on("exit", function(worker, code, signal) {
+    cluster.fork();
+  });
 }
 
-//Rate Limit
-const limiter = rateLimit({
-  max: 10000,
-  windowMs: 60 * 60 * 1000,
-  message: "Too many requests from this IP, please try again in an hour!",
-});
-app.use("/api", limiter);
+if (cluster.isWorker) {
+  const express = require("express");
+  const path = require("path");
+  const dotenv = require("dotenv");
+  const morgan = require("morgan");
+  const rateLimit = require("express-rate-limit");
+  const helmet = require("helmet");
+  const mongoSanitize = require("express-mongo-sanitize");
+  const xss = require("xss-clean");
+  const cookieParser = require("cookie-parser");
+  const compression = require("compression");
+  const cors = require("cors");
+  const app = express();
+  const apiRouter = require("./routes/apiRoutes.js");
+  const {
+    allOwnedGames,
+    getHiddenAchievementDataAndStore,
+    refreshDatabaseAndStore,
+  } = require("./controllers/cacheController.js");
+  const { log } = require("console");
+  const { LOG } = require("./helper/logger.js");
+  console.clear();
 
-//Cookie Parser
-app.use(express.json({ limit: "10kb" }));
-app.use(express.urlencoded({ extended: true, limit: "10kb" }));
-app.use(cookieParser());
+  //Load in config
+  dotenv.config();
 
-//Prevention
-// Data sanitization against NoSQL query injection
-app.use(mongoSanitize());
-
-// Data sanitization against XSS
-app.use(xss());
-
-//All Routes
-//Handle all API Routes
-app.use("/api", apiRouter);
-
-//Generic Route - To be handled at last
-app.get("/", (req, res) =>
-  res.json({
-    status: "success",
-    data: "HOMEPAGE - TO BE BUILT",
-  })
-);
-
-app.get("*", (req, res) => {
-  res.json({
-    status: "fail",
-    data: "PAGE NOT FOUND - 404",
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    if ("OPTIONS" == req.method) {
+      res.sendStatus(200);
+    } else {
+      next();
+    }
   });
-});
 
-setInterval(() => {
-  LOG("CACHING DATA - CURRENT TIME -> ", new Date().toISOString());
-  LOG("Getting data from Steam and storing...");
-  refreshDatabaseAndStore();
-}, 1000 * 60 * 60);
+  //Middleware Stack
+  app.use(compression());
+  app.enable("trust proxy");
+  app.options("*", cors());
+  app.use(cors());
+  app.use(express.static(path.join(__dirname, "public")));
+  app.set("view engine", "pug");
+  app.set("views", path.join(__dirname, "views"));
+  app.use(helmet());
+  if (process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"));
+  }
 
-const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => LOG(`Listening on port ${PORT} 👋`));
+  //Rate Limit
+  const limiter = rateLimit({
+    max: 10000,
+    windowMs: 60 * 60 * 1000,
+    message: "Too many requests from this IP, please try again in an hour!",
+  });
+  app.use("/api", limiter);
+
+  //Cookie Parser
+  app.use(express.json({ limit: "10kb" }));
+  app.use(express.urlencoded({ extended: true, limit: "10kb" }));
+  app.use(cookieParser());
+
+  //Prevention
+  // Data sanitization against NoSQL query injection
+  app.use(mongoSanitize());
+
+  // Data sanitization against XSS
+  app.use(xss());
+
+  //All Routes
+  //Handle all API Routes
+  app.use("/api", apiRouter);
+
+  //Generic Route - To be handled at last
+  app.get("/", (req, res) =>
+    res.json({
+      status: "success",
+      data: "HOMEPAGE - TO BE BUILT",
+    })
+  );
+
+  app.get("*", (req, res) => {
+    res.json({
+      status: "fail",
+      data: "PAGE NOT FOUND - 404",
+    });
+  });
+
+  setInterval(() => {
+    LOG("CACHING DATA - CURRENT TIME -> ", new Date().toISOString());
+    LOG("Getting data from Steam and storing...");
+    refreshDatabaseAndStore();
+  }, 1000 * 60 * 60);
+
+  const PORT = process.env.PORT || 8000;
+  app.listen(PORT, () => LOG(`Listening on port ${PORT} 👋`));
+}
--- helper/gamesHelper.js
+++ helper/gamesHelper.js
@@ -100,8 +100,7 @@ exports.checkSelectionCriteriaFulfilledForAchievement = (
   if (select === "all") {
     return true;
   } else if (select === "unlocked") {
-    console.log(achievement);
-    if (achievement.unlocked == 1) {
+    if (achievement.unlocked === 1) {
       return true;
     } else {
       return false;
